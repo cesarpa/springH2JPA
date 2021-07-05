6900b63c493813f05ab35c5580656d9818b64e42
unknown <cesar.1023@hotmail.com>
2021-07-05 11:44:10 -0500

    SJPAH2 addig get aliesn and get alien by path id

--- src/main/kotlin/com/cesarpa/controllers/AlienController.java
+++ src/main/kotlin/com/cesarpa/controllers/AlienController.java
@@ -4,9 +4,13 @@ import com.cesarpa.models.Alien;
 import com.cesarpa.repositories.AlienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 public class AlienController {
@@ -30,10 +34,24 @@ public class AlienController {
         ModelAndView mv = new ModelAndView("showAlien.jsp");
         mv.addObject(alienRepository.findById(id).orElse(new Alien()));
 
+        /*
         System.out.println(alienRepository.findByName("cesarpa"));
         System.out.println(alienRepository.findByIdGreaterThan(100));
         System.out.println(alienRepository.findByTechSorted("java"));
+         */
+
         return mv;
     }
 
+    @RequestMapping("/aliens")
+    @ResponseBody
+    public String getAliens() {
+        return alienRepository.findAll().toString();
+    }
+
+    @RequestMapping("/alien/{id}")
+    @ResponseBody
+    public String getAlienByPath(@PathVariable("id") Integer id ) {
+        return alienRepository.findById(id).orElse(new Alien()).toString();
+    }
 }
