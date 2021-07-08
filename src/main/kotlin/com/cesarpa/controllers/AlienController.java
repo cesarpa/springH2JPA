b9dbc7616e2605aecd182cc22b50e6d46b9e868a
unknown <cesar.1023@hotmail.com>
2021-07-08 00:31:37 -0500

    SBH2JPA using rest controller and using post mapping for create alien

--- src/main/kotlin/com/cesarpa/controllers/AlienController.java
+++ src/main/kotlin/com/cesarpa/controllers/AlienController.java
@@ -3,18 +3,13 @@ package com.cesarpa.controllers;
 import com.cesarpa.models.Alien;
 import com.cesarpa.repositories.AlienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
@@ -25,34 +20,25 @@ public class AlienController {
         return "home.jsp";
     }
 
-    @RequestMapping("/addAlien")
-    public String addAlien(Alien alien) {
+    @PostMapping("/alien")
+    public Alien addAlien(@RequestBody Alien alien) {
         alienRepository.save(alien);
-        return "home.jsp";
+        return alien;
     }
 
-    @RequestMapping("/getAlien")
+    @GetMapping("/getAlien")
     public ModelAndView getAlien(@RequestParam Integer id) {
         ModelAndView mv = new ModelAndView("showAlien.jsp");
         mv.addObject(alienRepository.findById(id).orElse(new Alien()));
-
-        /*
-        System.out.println(alienRepository.findByName("cesarpa"));
-        System.out.println(alienRepository.findByIdGreaterThan(100));
-        System.out.println(alienRepository.findByTechSorted("java"));
-         */
-
         return mv;
     }
 
     @RequestMapping(path = "/aliens", produces = {"application/json", "application/xml"})
-    @ResponseBody
     public List<Alien> getAliens() {
         return alienRepository.findAll();
     }
 
     @RequestMapping("/alien/{id}")
-    @ResponseBody
     public Optional<Alien> getAlienByPath(@PathVariable("id") Integer id) {
         return alienRepository.findById(id);
     }
